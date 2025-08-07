1998c097bd985272460c250fb7bfa76a7ee515ca
Juan Lozoya <jlozoya1@ucol.mx>
2025-08-07 01:03:14 -0600

    header fixies

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./Home";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home/>
+  },
+  {
+    path: '*',
+    element: <NotFound/>
   },
 ]);
 
